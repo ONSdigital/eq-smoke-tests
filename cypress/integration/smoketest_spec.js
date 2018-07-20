08dcbc585b418a7c8985431a3347f12bdef52e7a
SamGodwin2 <sam.godwin@ext.ons.gov.uk>
2018-07-20 13:06:01 +0100

    Implemented Cypress smoke-tests -Removed all refrences to Chimp tests. -Made the smoke-test more lightweight by slimming down the test spec. -Added a script that will run the smoke tests against a eq-compose build as part of the CI process. -During the above CI process a video will be saved to the cypress dashboard.

---
+++ cypress/integration/smoketest_spec.js
@@ -0,0 +1,68 @@
+import {
+    addQuestionnaire,
+    typeIntoDraftEditor,
+    testId,
+    signIn
+} from "../utils";
+
+describe("eq-services", () => {
+
+    describe("Questionnaire creation", () => {
+        it("Can create a questionnaire", () => {
+            cy.visit("/");
+            signIn();
+            addQuestionnaire("My Questionnaire Title");
+        });
+
+        it("Can name the Page and Section", () => {
+
+            cy.get(testId("page-item")).click();
+            typeIntoDraftEditor(
+                testId("txt-question-title", "testid"),
+                "This is Page 1"
+            );
+            cy.get(testId("side-nav")).should("contain", "This is Page 1");
+
+            cy.get(testId("nav-section-link")).click();
+
+            typeIntoDraftEditor(
+                testId("txt-section-title", "testid"),
+                "This is Section 1"
+            );
+
+            cy.get(testId("side-nav")).should("contain", "This is Section 1");
+        });
+
+        it("Can add a answer", () => {
+            cy.get(testId("page-item")).click();
+
+            cy.get(testId("btn-add-answer")).click();
+
+            cy.get(testId(`btn-answer-type-currency`)).click();
+
+            cy.get("[data-test='txt-answer-label']").type("This is an answer label");
+
+            cy.get("[data-test='txt-answer-description']").type("This is an answer description");
+        });
+    });
+
+    describe("Runner previewing", () => {
+
+        it("can switch to a Runner preview.", () => {
+            cy
+                .get(`[data-test="btn-preview"]`)
+                .invoke("removeAttr", "target")
+                .click();
+        });
+
+        it("Contains all the necessary fields in the correct formatting", () => {
+
+            cy.get(testId(`block-title`, "qa")).should("contain", "This is Section 1");
+
+            cy.get(testId(`question-title`, "qa")).should("contain", "This is Page 1");
+
+            cy.get(testId(`input-text`, "qa")).should("exist");
+        });
+    });
+
+});
--- tests/pages/runner/thank-you.page.js
+++
@@ -1,6 +0,0 @@
-class ThankYou {
-  static getHeading() {
-    return '#main > div:nth-child(1) > h1';
-  }
-}
-module.exports = ThankYou;
